e073db42609a31a61977fd4867a7ac915a8afd99
Obie-Wan <terrefactor@gmail.com>
2015-04-04 19:15:16 +0400

    control auto update

--- client/views/admin.js
+++ client/views/admin.js
@@ -1,64 +1,61 @@
-Meals = new Mongo.Collection("meals");
+//Meals = new Mongo.Collection("meals");
 
-if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.admin.helpers({
-    meals: function () {
-      // Show newest tasks first
-      return Meals.find({});
-    },
+// This code only runs on the client
+Template.admin.helpers({
+  meals: function () {
+    // Show newest tasks first
+    return Meals.find({});
+  },
 
-    mealsCount: function () {
-      return Meals.find().count();
-    }
-  });
+  mealsCount: function () {
+    return Meals.find().count();
+  }
+});
 
-  Template.admin.events({
+Template.admin.events({
+  "submit .remove-all": function (event) {
+    Meals.find().map( function(i) { Meals.remove(i._id) })
+  },
 
-    "submit .remove-all": function (event) {
-      Meals.find().map( function(i) { Meals.remove(i._id) })
-    },
+  "submit .new-meal": function (event) {
+    // This function is called when the new task form is submitted
+    var text = event.target.meals_data.value;
 
-    "submit .new-meal": function (event) {
-      // This function is called when the new task form is submitted
-      var text = event.target.meals_data.value;
+    var lines = text.replace(/\r\n/g, "\n").split("\n");
+    var newLines, newValue, i;
 
-      var lines = text.replace(/\r\n/g, "\n").split("\n");
-      var newLines, newValue, i;
+  // Use the map() method of Array where available 
+    newLines = lines.map(function(i) {
+      var parts = i.split(",").map(function(j) {return j.trim()});
+      if ( parts.length == 5 )
+      {
+        var obj = {
+          name: parts[0],
+          nutrients: {
+            protein: parseFloat(parts[1]),
+            fat: parseFloat(parts[2]),
+            carbohydrates: parseFloat(parts[3]),
+            calories:parseFloat(parts[4])
+          }
+        };
 
-    // Use the map() method of Array where available 
-      newLines = lines.map(function(i) {
-        var parts = i.split(",").map(function(j) {return j.trim()});
-        if ( parts.length == 5 )
-        {
-          var obj = {
-            name: parts[0],
-            nutrients: {
-              protein: parseFloat(parts[1]),
-              fat: parseFloat(parts[2]),
-              carbohydrates: parseFloat(parts[3]),
-              calories:parseFloat(parts[4])
-            }
-          };
+        Meals.insert(obj);
+        return obj
+      }
+      else
+        {return parts.length + "/" + parts.join(",");}
+    });
+  
+    text.value = newLines.join("\r\n");
+    /*Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });*/
 
-          Meals.insert(obj);
-          return obj
-        }
-        else
-          {return parts.length + "/" + parts.join(",");}
-      });
-    
-      text.value = newLines.join("\r\n");
-      /*Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });*/
+    // Clear form
+    event.target.meals_data.value = newLines.join("\r\n");
 
-      // Clear form
-      event.target.meals_data.value = newLines.join("\r\n");
-
-      // Prevent default form submit
-      return false;
-    }
-  });
-}
+    // Prevent default form submit
+    return false;
+  }
+});
--- client/views/index.js
+++ client/views/index.js
@@ -1,26 +1,35 @@
-if (Meteor.isClient) {
-    Template.index.events({
-      "submit .new-diet": function (event) {
-        //diet_period
-        var period = 'day';
-        if (document.getElementById('weekly_diet').checked) {
-          period = 'week';
-        }
-        
-        initDaysCollection(period);
 
-        // save meal count state
-        daily_limits = document.getElementsByName('meals_limit');
-        for (var i = 0; i < daily_limits.length; i++) {
-            console.log(daily_limits[i].value);
+function updateView() {
+    //diet_period
+    var period = 'day';
+    if (document.getElementById('weekly_diet').checked) {
+      period = 'week';
+    }
+    
+    initDaysCollection(period);
 
-            if (daily_limits[i].checked) {
-                Session.set('meals_limit', parseInt(daily_limits[i].value));
-            }
+    // save meal count state
+    daily_limits = document.getElementsByName('meals_limit');
+    for (var i = 0; i < daily_limits.length; i++) {
+        if (daily_limits[i].checked) {
+            Session.set('meals_limit', parseInt(daily_limits[i].value));
         }
-        
-        // Prevent default form submit
-        return false;
-      }
-    });
+    }
 }
+
+
+Template.index.onRendered(function () {
+    updateView();
+});
+
+Template.index.events({
+
+  'change .formitem': function(event) {
+    updateView();
+  },
+
+  'submit .new-diet': function (event) {
+    updateView();
+    return false;
+  }
+});
