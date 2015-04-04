a2ff1e9e27f7bc49587ae72f0935e672a53219a7
Obie-Wan <terrefactor@gmail.com>
2015-04-04 16:38:36 +0400

    count & period filters

--- client/views/mealsList.js
+++ client/views/mealsList.js
@@ -1,5 +1,5 @@
 //Meals = new Meteor.Collection('meals');
 
 Template.mealsList.helpers({
-    meals: function () { return Meals.find(); }
+    meals: function () { return Meals.find({}, {limit: Session.get('meals_limit')}); }
 });
--- collections/days.js
+++ collections/days.js
@@ -1,1 +1,20 @@
-Days = new Meteor.Collection('days');
+Days = new Meteor.Collection(null);
+
+Periods = {
+	'day': ['На день'],
+	'week': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресение']
+};
+
+initDaysCollection = function(key) {
+	Days.remove({});
+
+	if (!(key in Periods)) {
+		console.log('Unknown period type', key);
+		return;
+	}
+
+	var days = Periods[key]; 
+	for (var i = 0; i < days.length; i++ ) {
+  		Days.insert({ title: days[i] });
+	}
+};
