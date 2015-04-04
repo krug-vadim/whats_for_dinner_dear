344ede1bb7cfbbd66ba3bd693716ea992e9abd01
Obie-Wan <terrefactor@gmail.com>
2015-04-04 17:12:51 +0400

    broken router tpls

--- client/routes.js
+++ client/routes.js
@@ -1,10 +1,11 @@
-Router.route('/admin', function () {
-  this.render('addMeals');
+Router.configure({
+  autoRender: false
 });
 
 Router.map(function () {
   this.route('index', { path: '/' });
-  this.route('addMeals', { path: '/admin' });
+  this.route('admin', { path: '/admin' });
+  this.route('contacts', { path: '/contacts' });
   /*this.route('/admin', function () {
     this.render('addMeals');
   });*/
