be153f395c365f8ca3dfd708f1587aca3c8308b8
Obie-Wan <terrefactor@gmail.com>
2015-04-05 08:22:22 +0400

    wurkz

--- client/views/index.js
+++ client/views/index.js
@@ -1,3 +1,21 @@
+function calcPortions(meals, resources) {
+  var i, j;
+
+    var days = Days.find().count();    
+    Meteor.call('mealsForDays', resources, 'cost', days, meals, function(err, data) {
+        //updateDays(data);        
+        console.log(data);
+        Session.set('qmeals', data);
+    });
+}
+
+function updateDays(data) {
+    var days = Days.find().fetch();    
+    for(var i=0; i < days.length; i++) {
+
+        //days[i][''].update({$set: {C:false}});
+    }
+}
 /*
     Update days view, according to user input.
 */
@@ -7,14 +25,19 @@ function updateView() {
     if (document.getElementById('weekly_diet').checked) {
       period = 'week';
     }
-    
+        
+    var resources = {
+      protein:       parseInt(document.getElementById('proteins-input').value),
+      fat:           parseInt(document.getElementById('fats-input').value),
+      carbohydrates: parseInt(document.getElementById('carbohydrates-input').value)
+    };
     var day_count = initDaysCollection(period);
 
     // save meal count state to session
     daily_limits = document.getElementsByName('meals_limit');
     for (var i = 0; i < daily_limits.length; i++) {
         if (daily_limits[i].checked) {
-            Session.set('meals_limit', parseInt(daily_limits[i].value) * day_count);
+            calcPortions(parseInt(daily_limits[i].value), resources);
             return;
         }
     }
@@ -25,11 +48,11 @@ Template.index.onRendered(function () {
 });
 
 Template.mealsList.helpers({
-    meals: function () { return Meals.find({}, {limit: Session.get('meals_limit')}); }
+    qmeals: function () { return Session.get('qmeals'); }
 });
 
 Template.index.events({
-  'change .formitem, click #create-new-diet': function (event) {
+  'change .formitem, click #create-new-diet, input.form-control': function (event) {
     updateView();
     return false;
   },
